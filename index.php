11df5e850c5e39ed65a1835e3fd73df88b4f0d2d
jamesrio2014 <97978444+jamesrio2014@users.noreply.github.com>
2022-02-01 05:39:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,9 @@
-<?php
-/**
- * Campaign: BrendaWashington
- * Created: 2022-01-31 21:31:54 UTC
- */
-
-require 'leadcloak-169ox635w5e9.php';
-
-// ---------------------------------------------------
-// Configuration
-
-// Set this to false if application is properly installed.
-$enableDebugging = false;
-
-// Set this to false if you won't want to log error messages
-$enableLogging = true;
-
-if ($enableDebugging) {
-	isApplicationReadyToRun();
-}
-
-if (isPost())
-{
-	// Allows for modded query strings
-	$myQueryString = getForwardedQueryString($_POST);
-
-	/**
-	 *  Add or Modify Query String Variables in the section below.
-	 *  WARNING: Variables with the same name will be re-written
-	 */
-    // Ex.: $myQueryString['my_custom_variable'] = 'my custom variable';
-
-	$data = httpRequestMakePayload($campaignId, $campaignSignature, $_POST);
-
-	$response = httpRequestExec($data);
-
-	httpHandleResponse($response, $enableLogging);
-
-	exit();
-}
-else
-{
-	try {
-		loadJavascript($campaignId);
-	} catch (Exception $e) {
-		exit();
-	}
-}
-?>
 
 <!doctype html>
 <html class="no-js" lang="de" dir="ltr">
   <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-    <style type="text/css"> html { display: none; }</style>
-	<script type="text/javascript" src="169ox635w5e9.js"></script>
     
           <link rel="preconnect" href="https://logx.optimizely.com"><script src="js/11707774521.js"></script>      
     <meta charset="utf-8">
